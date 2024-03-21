0b3278b0592033625884c73abab8b61a05f34d1b
xj63 <xj63work@163.com>
2024-03-21 13:07:02 +0800

    chore(format): change " to '

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import Layouts from 'vite-plugin-vue-meta-layouts'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import regexpPlugin from 'rollup-plugin-regexp'
 import * as mdicons from '@mdi/js'
-import viteCompression from "vite-plugin-compression";
+import viteCompression from 'vite-plugin-compression'
 
 const mdi: Record<string, string> = {}
 Object.keys(mdicons).forEach((key) => {
@@ -27,11 +27,11 @@ export default defineConfig({
   base: '/dashboard',
   plugins: [
     viteCompression({
-      filter: "/.(js|css|json|txt|html|ico|svg)(?.*)?$/i", // 需要压缩的文件
+      filter: '/.(js|css|json|txt|html|ico|svg)(?.*)?$/i', // 需要压缩的文件
       threshold: 1024, // 文件容量(字节)大于这个值进行压缩
       // algorithm: 'brotliCompress', // 使用brotli压缩算法
-      algorithm: "gzip", // 压缩方式
-      ext: "gz", // 后缀名
+      algorithm: 'gzip', // 压缩方式
+      ext: 'gz', // 后缀名
       deleteOriginFile: false, // 压缩后是否删除压缩源文件
     }),
     regexpPlugin({
