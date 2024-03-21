a92941f8bb3d5af33e4e1f55421a4bf858bf1ee9
xj63 <xj63work@163.com>
2024-03-21 13:16:49 +0800

    feat(device-table): new page add

---
+++ src/pages/device-table.vue
@@ -0,0 +1,160 @@
+<script setup lang="ts">
+import DialogConfirm from '@/components/DialogConfirm.vue'
+import type { DataTableHeaders } from '@/plugins/vuetify'
+
+definePage({
+  meta: {
+    icon: 'mdi-table',
+    title: 'Device Table',
+    drawerIndex: 3,
+  },
+})
+
+const search = ref('')
+const dialogDelete = ref<InstanceType<typeof DialogConfirm> | null>(null)
+function showDialogDelete(name: string) {
+  dialogDelete.value
+    ?.open('Are you sure you want to delete this dessert?')
+    .then(async (confirmed: boolean) => {
+      if (confirmed) {
+        try {
+          const index = desserts.value!.findIndex((v) => v.id === name)
+          desserts.value!.splice(index, 1)
+          Notify.success('Deleted')
+        } catch (e) {
+          Notify.error(e)
+        }
+      }
+    })
+}
+
+function timestampToDate(timestamp: number) {
+  return new Date(timestamp * 1000).toLocaleString()
+}
+
+const desserts = ref([
+  {
+    id: 'idn2',
+    status: {
+      online: true,
+      last_update: 1709110302,
+      battery: 49,
+      position: '40.7127763N, 74.0059742W',
+      status: true,
+      error: '',
+    },
+  },
+  {
+    id: 'hebac',
+    status: {
+      online: true,
+      last_update: 1709110381,
+      battery: 9,
+      position: '40.7127763N, 74.0059742W',
+      status: false,
+      error: 'need charge',
+    },
+  },
+  {
+    id: 'eebaf',
+    status: {
+      online: false,
+      last_update: 1709110381,
+      battery: 99,
+      position: '40.7127763N, 74.0059742W',
+      status: true,
+      error: '',
+    },
+  },
+])
+
+const headers: DataTableHeaders = [
+  { title: 'Device id', key: 'id' },
+  { title: 'Online', key: 'status.online' },
+  { title: 'Last update', key: 'status.last_update' },
+  { title: 'Battery', key: 'status.battery' },
+  { title: 'Position', key: 'status.position' },
+  { title: 'Status', key: 'status.status' },
+  { title: 'Actions', key: 'actions', sortable: false },
+]
+</script>
+
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-card>
+          <client-only>
+            <teleport to="#app-bar">
+              <v-text-field v-model="search" prepend-inner-icon="mdi-magnify" label="Search" single-line hide-details
+                density="compact" class="mr-2" rounded="xl" flat variant="solo" style="width: 250px"></v-text-field>
+            </teleport>
+          </client-only>
+          <v-data-table :headers="headers" :items="desserts" item-value="name" :search="search">
+            <!-- online -->
+            <template #item.status.online="{ item }">
+              <v-icon v-if="item.status.online" icon="mdi-cloud" color="success"></v-icon>
+              <v-icon v-else icon="mdi-cloud-off" color="error"></v-icon>
+            </template>
+
+            <!-- last update -->
+            <template #item.status.last_update="{ item }">
+              {{ timestampToDate(item.status.last_update) }}
+            </template>
+
+            <!-- battery -->
+            <template #item.status.battery="{ item }">
+              <v-icon v-if="item.status.battery > 95" icon="mdi-battery" color="green"></v-icon>
+              <v-icon v-else-if="item.status.battery > 85" icon="mdi-battery-90" color="green"></v-icon>
+              <v-icon v-else-if="item.status.battery > 75" icon="mdi-battery-80" color="green"></v-icon>
+              <v-icon v-else-if="item.status.battery > 65" icon="mdi-battery-70"></v-icon>
+              <v-icon v-else-if="item.status.battery > 55" icon="mdi-battery-60"></v-icon>
+              <v-icon v-else-if="item.status.battery > 45" icon="mdi-battery-50"></v-icon>
+              <v-icon v-else-if="item.status.battery > 35" icon="mdi-battery-40"></v-icon>
+              <v-icon v-else-if="item.status.battery > 25" icon="mdi-battery-30"></v-icon>
+              <v-icon v-else-if="item.status.battery > 15" icon="mdi-battery-20" color="red"></v-icon>
+              <v-icon v-else-if="item.status.battery > 5" icon="mdi-battery-10" color="red"></v-icon>
+              <v-icon v-else icon="mdi-battery-outline" color="red"></v-icon>
+              {{ item.status.battery }}%
+            </template>
+
+            <!-- position -->
+            <template #item.status.position="{ item }">
+              {{ item.status.position }}
+            </template>
+
+            <!-- status -->
+            <template #item.status.status="{ item }">
+              <v-chip v-if="item.status.status" color="green">OK</v-chip>
+              <v-chip v-else color="red"> {{ item.status.error }} </v-chip>
+            </template>
+
+            <!-- action -->
+            <template #item.actions="{ item }">
+              <v-defaults-provider :defaults="{
+                VBtn: {
+                  size: 20,
+                  rounded: 'sm',
+                  variant: 'text',
+                  class: 'ml-1',
+                  color: '',
+                },
+                VIcon: {
+                  size: 20,
+                },
+              }">
+                <v-tooltip location="top">
+                  <template #activator="{ props }">
+                    <v-btn icon="mdi-delete-outline" v-bind="props" @click.stop="showDialogDelete(item.id)" />
+                  </template>
+                  <span>Delete</span>
+                </v-tooltip>
+              </v-defaults-provider>
+            </template>
+          </v-data-table>
+          <DialogConfirm ref="dialogDelete" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
