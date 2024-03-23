d6ecc4035704f43cfc1f86b7d55178f45a68dcac
xj63 <xj63work@163.com>
2024-03-23 12:35:17 +0800

    test(e2e): remove not have element

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test'
 
 test('example', async ({ page }) => {
   await page.goto('/')
-  await page.getByText('Opinionated Starter Template').isVisible()
   await page.getByLabel("What's your name?").fill('kingyue')
   await page.getByRole('button', { name: 'Confirm', exact: true }).click()
   const locator = page.getByText('Hi, kingyue!')
